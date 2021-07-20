8bfa21725ad1f8ab574e30c91778b6e8d578fd0a
Dinoryong <youngzi.koo@iamcraft.dev>
2021-07-20 16:41:22 +0900

    Time: 60 ms (76.87%), Space: 15.2 MB (60.32%) - LeetHub

---
+++ two-sum/two-sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums_map = {}
+
+        # integrate for statements
+        for i, num in enumerate(nums):
+            if target - num in nums_map:
+                return [nums_map[target - num], i]
+            nums_map[num] = i
