99cc11265c0f3a361a121dd871a1ca5436605e79
Dinoryong <youngzi.koo@iamcraft.dev>
2022-01-13 22:55:20 +0900

    Time: 32 ms (61.88%), Space: 14.2 MB (44.93%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -0,0 +1,9 @@
+class Solution:
+    def climbStairs(self, n):
+        if n == 1:
+            return 1
+        res = [0 for i in range(n)]
+        res[0], res[1] = 1, 2
+        for i in range(2, n):
+            res[i] = res[i-1] + res[i-2]
+        return res[-1]
