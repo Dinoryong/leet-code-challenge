4ccf5de332a7bcd805371972b5180c343bbf2d4d
Dinoryong <youngzi.koo@iamcraft.dev>
2022-01-23 10:30:57 +0900

    Time: 74 ms (73.60%), Space: 14.9 MB (51.55%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -1,12 +1,11 @@
 ''' 
 [intervals, ]
 sol 1. simply go through start~end and check if capacity exceeds
+=> O(Nlog(N))
 
+sol 2. Bucket Sort
+=> O (max(N,1001))
 
-sol 2.
-
-Input: trips = [[2,1,5],[3,3,7]], capacity = 4
-Output: false
 '''
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
@@ -26,3 +25,19 @@ class Solution:
                 return False
 
         return True
+    
+# sol 2
+# class Solution:
+#     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+#         timestamp = [0] * 1001
+#         for trip in trips:
+#             timestamp[trip[1]] += trip[0]
+#             timestamp[trip[2]] -= trip[0]
+
+#         used_capacity = 0
+#         for passenger_change in timestamp:
+#             used_capacity += passenger_change
+#             if used_capacity > capacity:
+#                 return False
+
+#         return True
