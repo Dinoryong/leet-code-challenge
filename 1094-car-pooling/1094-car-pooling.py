6e5134089f61c4103f3a0b30881176943ce1be72
Dinoryong <youngzi.koo@iamcraft.dev>
2022-01-23 10:38:35 +0900

    Time: 77 ms (71.73%), Space: 14.9 MB (19.42%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -27,17 +27,17 @@ class Solution:
         return True
     
 # sol 2
-# class Solution:
-#     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-#         timestamp = [0] * 1001
-#         for trip in trips:
-#             timestamp[trip[1]] += trip[0]
-#             timestamp[trip[2]] -= trip[0]
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        timestamp = [0] * 1001
+        for trip in trips:
+            timestamp[trip[1]] += trip[0]
+            timestamp[trip[2]] -= trip[0]
 
-#         used_capacity = 0
-#         for passenger_change in timestamp:
-#             used_capacity += passenger_change
-#             if used_capacity > capacity:
-#                 return False
+        used_capacity = 0
+        for passenger_change in timestamp:
+            used_capacity += passenger_change
+            if used_capacity > capacity:
+                return False
 
-#         return True
+        return True
