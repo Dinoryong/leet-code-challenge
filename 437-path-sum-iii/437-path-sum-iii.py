01d4b8fa117fe50daf177352d070fe8afabe92bc
Dinoryong <youngzi.koo@iamcraft.dev>
2022-02-21 10:54:02 +0900

    Time: 52 ms (86.79%), Space: 15.6 MB (41.74%) - LeetHub

---
+++ 437-path-sum-iii/437-path-sum-iii.py
@@ -0,0 +1,43 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: TreeNode, sum: int) -> int:
+        def preorder(node: TreeNode, curr_sum) -> None:
+            nonlocal count
+            if not node:
+                return 
+            
+            # current prefix sum
+            curr_sum += node.val
+            
+            # here is the sum we're looking for
+            if curr_sum == k:
+                count += 1
+            
+            # number of times the curr_sum − k has occurred already, 
+            # determines the number of times a path with sum k 
+            # has occurred up to the current node
+            count += h[curr_sum - k]
+            
+            # add the current sum into hashmap
+            # to use it during the child nodes processing
+            h[curr_sum] += 1
+            
+            # process left subtree
+            preorder(node.left, curr_sum)
+            # process right subtree
+            preorder(node.right, curr_sum)
+            
+            # remove the current sum from the hashmap
+            # in order not to use it during 
+            # the parallel subtree processing
+            h[curr_sum] -= 1
+            
+        count, k = 0, sum
+        h = defaultdict(int)
+        preorder(root, 0)
+        return count
