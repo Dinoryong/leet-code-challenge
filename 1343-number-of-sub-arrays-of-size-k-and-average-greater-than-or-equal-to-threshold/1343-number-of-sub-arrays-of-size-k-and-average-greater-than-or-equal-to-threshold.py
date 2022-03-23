cd371d7ee4568aea7f9cb356b36a018980ae4a34
Dinoryong <youngzi.koo@iamcraft.dev>
2022-03-23 22:54:44 +0900

    Time: 1032 ms (24.39%), Space: 27.3 MB (34.47%) - LeetHub

--- 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
+++ 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
@@ -1,15 +1,19 @@
 class Solution:
-    def numOfSubarrays(self, arr: List[int], k: int, threshold: int) -> int:
-        if len(arr) < k:
-            return 0
-        threshold = threshold * k
-        ans = 0
-        k_sum = sum(arr[:k])
-        if k_sum >= threshold:
-            ans += 1
-        for i in range(k, len(arr)):
-            k_sum -= arr[i-k]
-            k_sum += arr[i]
-            if k_sum >= threshold:
-                ans += 1
-        return ans
+    def numOfSubarrays(self, arr, k, threshold):
+        windowStart = 0
+        max_avg = 0
+        avg = 0
+        c=0
+        result = []
+        windowSum = 0
+        for windowEnd in range(len(arr)):
+            windowSum += arr[windowEnd]
+            if((windowEnd)>=k-1):
+                avg = windowSum//k
+                result.append(avg)
+                windowSum -= arr[windowStart]
+                windowStart += 1
+        for i in range(len(result)):
+            if(result[i]>=threshold):
+                c=c+1
+        return c
