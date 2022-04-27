b44d9d3595f67aa92896469cf17486429279cc83
Dinoryong <youngzi.koo@iamcraft.dev>
2022-04-27 23:23:59 +0900

    Time: 42 ms (58.99%), Space: 14.3 MB (7.87%) - LeetHub

---
+++ 91-decode-ways/91-decode-ways.py
@@ -0,0 +1,24 @@
+class Solution:
+
+    @lru_cache(maxsize=None)
+    def recursiveWithMemo(self, index, s) -> int:
+        # If you reach the end of the string
+        # Return 1 for success.
+        if index == len(s):
+            return 1
+
+        # If the string starts with a zero, it can't be decoded
+        if s[index] == '0':
+            return 0
+
+        if index == len(s)-1:
+            return 1
+        
+        answer = self.recursiveWithMemo(index + 1, s)
+        if int(s[index : index + 2]) <= 26:
+            answer += self.recursiveWithMemo(index + 2, s)
+
+        return answer
+
+    def numDecodings(self, s: str) -> int:
+        return self.recursiveWithMemo(0, s)
