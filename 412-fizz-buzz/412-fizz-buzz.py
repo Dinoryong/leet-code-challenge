45ae42089ea317f61438ba767ef1786c2c734644
Dinoryong <youngzi.koo@iamcraft.dev>
2022-06-10 01:21:22 +0900

    Time: 83 ms (11.66%), Space: 15.2 MB (16.45%) - LeetHub

--- 412-fizz-buzz/412-fizz-buzz.py
+++ 412-fizz-buzz/412-fizz-buzz.py
@@ -2,23 +2,19 @@ class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
         answer = []
 
+        fizz_buzz_dict = {3: "Fizz", 5: "Buzz"}
         
         for num in range(1, n+1):
-            
-            divisible_by_3 = (num % 3 == 0)
-            divisible_by_5 = (num % 5 == 0)
 
             num_ans_str = ""
 
-            if divisible_by_3:
-                num_ans_str += "Fizz"
-
-            if divisible_by_5:
-                num_ans_str += "Buzz"
-
+            for key in fizz_buzz_dict.keys():
+                if num % key == 0:
+                    num_ans_str += fizz_buzz_dict[key]
+            
             if not num_ans_str:
                 num_ans_str = str(num)
-
+            
             answer.append(num_ans_str)
-        
+            
         return answer
