f944bf99ece8d54cb9f2c50137d8b594b6313f09
Dinoryong <youngzi.koo@iamcraft.dev>
2022-06-17 23:56:23 +0900

    Time: 1992 ms (31.37%), Space: 13.8 MB (90.69%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,21 +1,22 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        if len(s) < 2:
-            return s
-        start = 0
-        maxLen = 1
-        i = 0
+        """
+        Consider each character in s as the centre of a palindrome.
+        Check for the longest possible odd-length and even-length palindrome; store the longest palindrome
+        """
 
-        while i < len(s):
-            l = i
-            r = i
-            while r < len(s) - 1 and s[r] == s[r+1]:
-                r += 1
-            i = r + 1
-            while r < len(s)-1 and l > 0 and s[r+1] == s[l-1]:
-                l -= 1
-                r += 1
-            if maxLen < r - l + 1:
-                start = l
-                maxLen = r - l + 1
-        return s[start: start + maxLen]
+        res, len_res, len_s = 0, 0, len(s)
+        for i in range(len_s):
+           
+            odd, len_odd = i, 1
+            for j in range(min(i, len_s-i-1)):   
+                if s[i-j-1] != s[i+j+1]:        
+                    break                        
+                odd, len_odd = odd-1, len_odd+2  
+            even, len_even = i, 0
+            for j in range(min(i, len_s-i)):
+                if s[i-j-1] != s[i+j]:
+                    break
+                even, len_even = even-1, len_even+2  
+            len_res, res = max((len_res, res), (len_odd, odd), (len_even, even))
+        return s[res:res+len_res]
