1a4479ab509426a10f5f6014add4ca3999763566
Dinoryong <youngzi.koo@iamcraft.dev>
2022-06-18 23:48:14 +0900

    Time: 62 ms (95.40%), Space: 26.3 MB (50.82%) - LeetHub

---
+++ 187-repeated-dna-sequences/187-repeated-dna-sequences.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        L, n = 10, len(s)     
+        seen, output = set(), set()
+
+        # iterate over all sequences of length L
+        for start in range(n - L + 1):
+            tmp = s[start:start + L]
+            if tmp in seen:
+                output.add(tmp[:])
+            seen.add(tmp)
+        return output
