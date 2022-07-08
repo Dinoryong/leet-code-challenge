73ecfbec04b052c516860de8090067c5ae1dbf58
Dinoryong <youngzi.koo@iamcraft.dev>
2022-07-08 12:10:01 +0900

    Time: 1234 ms (31.15%), Space: 27.5 MB (54.71%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.py
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -1,16 +1,15 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        # M1: Using 2 loops with i from 0 to len and j from i + 1 to len
-        # O(n^2) Time Complexity - Time Exceeded for large inputs and O(1) Space Complexity
+        maxarea = 0
+        left = 0
+        right = len(height) - 1
         
-        # M2: Using 2 pointers
-        # O(n) Time Complexity and O(1) Space Complexity
-        l, r = 0, len(height)-1
-        max_area = 0
-        while l<r:
-            max_area = max((r-l)*min(height[l], height[r]), max_area)
-            if height[l] < height[r]:
-                l += 1
+        while left < right:
+            width = right - left
+            maxarea = max(maxarea, min(height[left], height[right]) * width)
+            if height[left] <= height[right]:
+                left += 1
             else:
-                r -= 1
-        return max_area
+                right -= 1
+                
+        return maxarea
